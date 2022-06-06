aac2f57574f3ee79878757478cf4c57d04c1dc8d
George Thomas <george.thomas@libertymutual.com>
2022-06-06 09:11:58 -0400

    am662022

--- solar.py
+++ solar.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import requests
-from requests.auth import HTTPBasicAuth
 from datetime import datetime, date
-from pprint import pprint
+
+# from requests.auth import HTTPBasicAuth
+# from pprint import pprint
 
 # sets date and time variables
 from_date = date.today()
@@ -10,9 +11,21 @@ to_date = date.today()
 timenow = datetime.now()
 current_time = timenow.strftime("%H:%M:%S")
 
-# sets basic auth variables for astronomyapi.com cal
+# sets basic auth variables for astronomyapi.com
 ASTRONOMYAPI_ID = "0676a500-7552-477c-9610-b4dd6d813f76"
 ASTRONOMYAPI_SECRET = "1daa70b3767f42843bf372bed31cbc5aab65ddb3b7ed08ec9e8e8000c81167859d95e6c33fc6c473235c2e6c1c64399248555972d50046e050b073161eef3ba00b0749294b9682d3875e731c8fe23802b5dd0a773a5df6cf4a6687c181372b2f519db82d347f3fc122f1d0f3297ed127"
+
+# set some print colors
+def prRed(skk):
+    print("\033[91m {}\033[00m".format(skk))
+
+
+def prGreen(skk):
+    print("\033[92m {}\033[00m".format(skk))
+
+
+def prYellow(skk):
+    print("\033[93m {}\033[00m".format(skk))
 
 
 def get_observer_location():
@@ -68,7 +81,7 @@ def get_sun_position(latitude, longitude, elevation):
 
 def print_position(azimuth, altitude):
     # Prints the position of the sun in the sky using the supplied coordinates
-    print(
+    prGreen(
         f"The Sun is currently at: {azimuth} degrees azimuth, {altitude} degrees altitude"
     )
 
