939767d8b61c07b0dcec7726980c6efd014833cf
George Thomas <george.thomas@libertymutual.com>
2022-06-07 13:38:46 -0400

    added a test

--- pytest-solar.py
+++ pytest-solar.py
@@ -1,7 +1,14 @@
 # adding pytesting for solar.py script
 # test
 from solar import print_position
+from solar import get_observer_location
+from solar import get_elevation
+from solar import get_sun_position
 
 
 def test_print_position():
     pass
+
+
+def test_get_observer_location():
+    result = get_observer_location()
