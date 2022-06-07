c0e3028baba22a676035ec2deba779ce8cf9c690
George Thomas <george.thomas@libertymutual.com>
2022-06-07 09:30:26 -0400

    am672022

--- solar.py
+++ solar.py
@@ -15,8 +15,9 @@ current_time = timenow.strftime("%H:%M:%S")
 ASTRONOMYAPI_ID = "0676a500-7552-477c-9610-b4dd6d813f76"
 ASTRONOMYAPI_SECRET = "1daa70b3767f42843bf372bed31cbc5aab65ddb3b7ed08ec9e8e8000c81167859d95e6c33fc6c473235c2e6c1c64399248555972d50046e050b073161eef3ba00b0749294b9682d3875e731c8fe23802b5dd0a773a5df6cf4a6687c181372b2f519db82d347f3fc122f1d0f3297ed127"
 
-# set some print colors
+
 def prRed(skk):
+    # set some print colors
     print("\033[91m {}\033[00m".format(skk))
 
 
